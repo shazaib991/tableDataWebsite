a8c4ba014e3cd95736dafbe96255b48ce678d77b
shazaib munawar <shazaib424647@gmail.com>
2022-07-08 23:46:54 +0500

    update

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import { data } from "./data.js";
 let tr = document.getElementById("tr");
 let tbody = document.getElementById("tbody");
 
+//iterate over keys of data which should be headers of table using foreach on keys of data
 Object.keys(data).forEach((element) => {
     let td = document.createElement("td");
     td.innerText = element;
     tr.appendChild(td);
 });
 
+//iterate over values of data which is about 17 and iterate over each of the values in data using foreach
 for (let i = 0; i < Object.values(data).length; i++) {
     let tr = document.createElement("tr");
     tr.setAttribute("id", "tr");
